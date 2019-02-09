14adac47a219427845b6d0c69c51887f61e82cd6
thosagoraymond <thosagoraymond@gmail.com>
2019-02-09 11:55:45 +0200

    adding stack navigation structure

--- App.js
+++ App.js
@@ -1,16 +1,122 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View , Button } from 'react-native';
+import Icon from '@expo/vector-icons/Ionicons'
 
-export default class App extends React.Component {
+import {
+   createSwitchNavigator, 
+   createAppContainer, 
+   createDrawerNavigator,
+   createBottomTabNavigator,
+   createStackNavigator
+   } from 'react-navigation'; 
+
+class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>This is our first page in the app</Text>
+    return <AppContainer/>;
+  }
+}
+export default App;
+
+//Below are the screen components that can be exported to screens
+class WelcomeScreen extends Component{
+  render(){
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+         <Button title="Login" 
+          onPress={() => this.props.navigation.navigate('Dashboard')}
+          />
+
+         <Button title="Sign Up" 
+          onPress={() => alert('button pressed')}/>
       </View>
-    );
+    )
   }
 }
 
+class DashboardScreen extends Component{
+  render(){
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>DashboardScreen</Text>
+      </View>
+    )
+  }
+}
+
+class Feed extends Component{
+  render(){
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Feed</Text>
+      </View>
+    )
+  }
+}
+
+class Settings extends Component{
+  render(){
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Settings</Text>
+      </View>
+    )
+  }
+}
+
+class Profile extends Component{
+  render(){
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Profile</Text>
+      </View>
+    )
+  }
+}
+
+//Bottom tab navigator
+const DashboardTabNavigator = createBottomTabNavigator({
+  Feed,
+  Profile,
+  Settings
+},{
+  navigationOptions:({navigation})=>{
+    const { routeName } = navigation.state.routes
+      [navigation.state.index];
+        return{
+          headerTitle: routeName
+        };
+  }
+});
+
+const DashboardStackNavigator = createStackNavigator({
+  DashboardTabNavigator:DashboardTabNavigator
+},
+{
+  defaultNavigationOptions: ({navigation}) => {
+    return{
+      headerLeft: <Icon style={{paddingLeft:10 }}
+      onPress={()=> navigation.openDrawer()} 
+      name="md-menu" size={30} />
+    };
+  }
+});
+
+const AppDrawerNavigator = createDrawerNavigator({
+  Dashboard:{
+    screen:DashboardStackNavigator
+  }
+});
+
+const AppSwitchNavigator = createSwitchNavigator({
+  Welcome:{screen: WelcomeScreen},
+  Dashboard:{screen: AppDrawerNavigator}
+}); 
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
+
+
+
+//Style starts here
 const styles = StyleSheet.create({
   container: {
     flex: 1,
