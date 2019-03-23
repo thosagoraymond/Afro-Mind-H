572f159cac412f9ad46ce40700e27f658f7054b1
thosagoraymond <thosagoraymond@gmail.com>
2019-03-23 13:01:12 +0200

    added instructors cards

--- screens/instructors.js
+++ screens/instructors.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Image } from 'react-native';
+import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+const cards = [
+  {
+    name: 'Raymond Mogodi',
+    image: require('../assets/testing.png'),
+    participant: '11',
+    Activities :  'Yoga , Singing and Dancing',
+    // text: 'Card One',
+  },
+  {
+    text: 'Card Two',
+    name: 'Two',
+    image: require('../assets/splash1.png')
+  }
+];
 
 class instructors extends Component{
   render(){
     return(
-      <View style = {styles.container}>
-          <View style={{ flex: 1 , alignItems: 'center' ,paddingTop: 10}}>
-              <Text>Instructors page</Text>
-          </View>
-          
-      </View>
-    )
+      <Container>
+        <Header />
+        <View>
+          <DeckSwiper
+            dataSource={cards}
+            renderItem={item =>
+              <Card style={{ elevation: 3 }}>
+                <CardItem>
+                  <Left>
+                    <Body>
+                      <Text>{item.name}</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                
+                <CardItem cardBody>
+                  <Image style={{ height: 180, flex: 1 }} source={item.image} />
+                </CardItem>
+
+                <CardItem>
+                  <Icon name="heart" style={{ color: '#ED4A6A' }} />
+                  <Text>participants : {item.participant}</Text>
+                </CardItem>
+
+                <CardItem>
+                    <Text> Meetings </Text>
+
+                    <Text note> Virtual meeting : </Text>
+
+                    <Text note >Physical meeting : </Text >
+                </CardItem>
+
+                <CardItem>
+                  <Text>Therapist Activities</Text>
+                </CardItem>
+
+                <CardItem> 
+                  <Icon name="heart" style={{ color: '#ED4A6A' }} />
+                  <Text>{item.Activities}</Text>
+                </CardItem>
+                
+              </Card>
+            }
+          />
+        </View>
+      </Container>
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    justifyContent: "flex-end",
-    alignItems: 'center'
-  }
-})
 export default instructors;
